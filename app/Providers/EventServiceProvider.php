07f36b0ada3a55f6938ed65c374e3c1f3b627cc1
Eduardo Teixeira <dunirvana@gmail.com>
2023-02-22 17:09:37 -0300

    Configurada a execução dos listeners com base no evento se série criada

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\SeriesCreated;
+use App\Listeners\EmailUsersAboutSeriesCreated;
+use App\Listeners\LogSeriesCreated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +20,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        SeriesCreated::class => [
+            EmailUsersAboutSeriesCreated::class,
+            LogSeriesCreated::class,
         ],
     ];
 
